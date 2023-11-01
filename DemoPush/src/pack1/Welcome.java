0fb640d381753f8841d89fdf0bcd766809fec674
Charitha_Reddy <charithareddycr3@gmail.com>
2023-11-01 17:04:59 +0530

    4th commit

--- DemoPush/src/pack1/Welcome.java
+++ DemoPush/src/pack1/Welcome.java
@@ -7,6 +7,7 @@ public class Welcome {
 		System.out.println("Working with eclipse");
 		System.out.println("Working with eclipse line 2");
 		System.out.println("Working with eclipse for pull operstion");
+		System.out.println("Working with eclipse for pulll operstion");
 
 	}
 
